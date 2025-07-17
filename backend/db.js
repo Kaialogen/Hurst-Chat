8157e0e486b0b06a16b5cf65c0c3609e6a47ad09
Kaialogen <kaialogen@protonmail.com>
2025-07-17 19:39:38 +0100

    chore: Update .gitignore and add dotenv dependency for environment variable management

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
   host: "localhost",
   port: 5432,
-  user: "postgres",
-  password: "postgres",
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
   database: "Hurst_Chat",
 });
 

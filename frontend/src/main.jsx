aa8f73fc095a56e05dd0691d01f3e754e6ae1c2f
Kaialogen <kaialogen@protonmail.com>
2025-07-20 15:13:55 +0100

    feat: Added React Router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import "./style.css";
 
-createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
